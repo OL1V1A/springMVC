d35ba16565b64bbf439c56277c8a36ad50c83c24
OL1V1A <foreverdnmy@163.com>
2019-06-23 22:37:49 +0800

    AbstractQueuedSynchronizer同步器

---
+++ src/main/java/com/lwj/test/TestLock.java
@@ -0,0 +1,42 @@
+package com.lwj.test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestLock {
+
+    private int value;
+    private  MyLock lock = new MyLock();
+
+    private int next(){
+        lock.lock();
+        try {
+            TimeUnit.MILLISECONDS.sleep(400);
+            return value++;
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void a(){
+        lock.lock();
+        System.out.println("a");
+        b();
+        lock.unlock();
+    }
+
+    public void b(){
+        lock.lock();
+        System.out.println("b");
+        lock.unlock();
+    }
+
+    public static void main(String[] args) {
+        TestLock l = new TestLock();
+        new Thread(() -> {
+            l.a();
+        }).start();
+
+    }
+}
